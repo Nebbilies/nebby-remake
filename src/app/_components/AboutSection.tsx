39340bb839c75a2e841ebccde1f1646313ee5d4a
nebbilies <hirohamada17092005@gmail.com>
2026-01-14 14:45:45 +0700

    feat: home page basic layout

---
+++ src/app/_components/AboutSection.tsx
@@ -0,0 +1,112 @@
+
+'use client';
+
+import { motion } from 'framer-motion';
+import Image from "next/image";
+
+
+export default function AboutSection() {
+  const orbitRadius = 200;
+  const icons = [
+    { name: 'JavaScript', src: '/icons/javascript.svg' },
+    { name: 'Figma', src: '/icons/figma.svg' },
+    { name: 'React', src: '/icons/react.svg' },
+    { name: 'Node.js', src: '/icons/nodejs.svg' },
+  ]
+
+  return (
+      <div className="max-w-7xl mx-auto px-5 py-20 min-h-screen h-screen">
+        <div className="flex items-center gap-8 mb-8">
+          <div className="relative overflow-hidden">
+            <motion.div
+                className="absolute inset-0 bg-accent z-10"
+                initial={{ scaleX: 1 }}
+                whileInView={{ scaleX: 0 }}
+                transition={{ duration: 0.4, ease: "easeInOut" }}
+                style={{ transformOrigin: "left" }}
+            />
+
+            <motion.h2
+                className="text-6xl font-bold whitespace-nowrap"
+                initial={{ y: 50, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.4, delay: 0.2, type: "tween", ease: "easeOut"  }}
+            >
+              About<span className="text-accent">.</span>
+            </motion.h2>
+          </div>
+          <div className="flex-1 h-[2px] bg-neutral-content"></div>
+        </div>
+        <div className={"w-full h-full flex flex-col md:flex-row gap-10 md:gap-20"}>
+          <div className={"relative w-1/2 h-full flex justify-center items-center"}>
+            <div className={'absolute w-100 h-100 border-dashed border-neutral-content border-3 rounded-full'}/>
+            <div className={'absolute w-103 h-103 border-dashed border-neutral-content/50 border-2 rounded-full'}/>
+
+            <motion.div
+                className="absolute"
+                style={{ width: `${orbitRadius * 2}px`, height: `${orbitRadius * 2}px` }}
+                animate={{ rotate: 360 }}
+                transition={{
+                  duration: 20,
+                  repeat: Infinity,
+                  ease: "linear",
+                }}
+            >
+              {icons.map((icon, index) => {
+                const angle = (360 / icons.length) * index;
+                const radians = (angle * Math.PI) / 180;
+                const x = orbitRadius * Math.cos(radians);
+                const y = orbitRadius * Math.sin(radians);
+
+                return (
+                    <motion.div
+                        key={icon.name}
+                        className="absolute top-15 w-16 h-16 border-2 border-neutral-content rounded-full bg-base-100 flex justify-center items-center shadow-lg"
+                        style={{
+                            top: `${orbitRadius + y - 30}px`,
+                            left: `${orbitRadius + x - 30}px`,
+                        }}
+                        animate={{ rotate: -360 }}
+                        transition={{
+                          duration: 20,
+                          repeat: Infinity,
+                          ease: "linear",
+                        }}
+                    >
+                      <Image
+                          src={icon.src}
+                          alt={icon.name}
+                          width={40}
+                          height={40}
+                          className={'text-neutral-content'}
+                      />
+                    </motion.div>
+                )
+              })}
+            </motion.div>
+            <Image
+                src={"/image/avatar.gif"}
+                alt={"avatar"}
+                width={250}
+                height={250}
+                className={"mask mask-circle"}
+            />
+          </div>
+            <div className={'flex flex-1 flex-col justify-center'}>
+                <h3 className="text-4xl font-bold mb-5">
+                    A bit about <span className={'text-accent'}>me</span>.
+                </h3>
+                <p className="text-lg mb-4">
+                    I&#39;m Nebby, a third-year student at University of Information Technology, VNU-HCM, specializing in Software Engineering. I specialize in the <strong className={'text-accent'}>frontend</strong>, primarily using React and Next.js, but love to use whatever tools are right for the job.
+                </p>
+                <p className="text-lg mb-4">
+                    I&#39;m currently learning about backend along with DevOps to become a more flexible developer. I also enjoy surfing the web for new technologies and best practices to improve my skills.
+                </p>
+                <p className="text-lg">
+                    Outside of coding, I love exploring <strong className={'text-accent'}>Japanese culture & language</strong>, listening to music, and playing rhythm games.
+                </p>
+            </div>
+        </div>
+      </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,37 @@
 import Image from "next/image";
+import {motion} from "framer-motion";
+import AboutSection from "@/app/_components/AboutSection";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      <div className={'relative min-h-screen'}>
+          <div
+              className="fixed -z-10 opacity-10 inset-0 bg-cover bg-center grayscale"
+              style={{ backgroundImage: 'url("/image/background/our-identity-flip.jpg")' }}
+          />
+          <div
+              className="fixed inset-0 opacity-40"
+              style={{
+                  backgroundImage: 'radial-gradient(circle, rgba(255,255,255,0.15) 1.4px, transparent 1px)',
+                  backgroundSize: '20px 20px'
+              }}
+          />
+          <div className="relative h-screen items-center justify-center w-full bg-cover"
+               style={{backgroundImage: 'url("/image/background/our-identity-flip.jpg")'}}>
+              <div className="absolute w-full h-full bg-gradient-to-r from-black/90 from-25% via-black/75 via-50% to-transparent to-100% opacity-90 -z-0"></div>
+              <div className="text-primary-content w-full h-full flex z-10 relative">
+                  <div className={'w-1/2 flex flex-col justify-center items-center h-full'}>
+                      <div className="max-w-xl">
+                          <h1 className="mb-5 text-7xl font-bold">Hi, <span className={'text-accent'}>Nebby </span> です。</h1>
+                          <h3 className="text-4xl font-bold mb-10">
+                              I <span className={'text-accent'}>build</span> things for the web.
+                          </h3>
+                          <button className="btn btn-primary btn-xl">Get Started</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <AboutSection/>
+      </div>
   );
 }
