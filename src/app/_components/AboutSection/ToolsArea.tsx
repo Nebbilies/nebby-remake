5ab2b416dbe3bd00963fbf124e61dfcb29d1f78f
nebbilies <hirohamada17092005@gmail.com>
2026-01-15 11:57:13 +0700

    feat: add ToolsArea component

---
+++ src/app/_components/AboutSection/ToolsArea.tsx
@@ -0,0 +1,17 @@
+export default function ToolsArea() {
+    const tools = ["Javascript", "TypeScript", "React", "Next.js", "ShadcnUI", "DaisyUI", "Node.js", "Express", "Figma", "Git"];
+    return (
+        <div className={'w-full'}>
+            <h3 className={'text-3xl font-bold mb-5'}>Things I&#39;ve <span className={'text-accent'}>worked</span> with.</h3>
+            <div className={'flex flex-wrap w-full gap-2'}>
+                {tools.map((tool) => {
+                    return (
+                        <div className={'badge badge-lg badge-neutral font-semibold'} key={tool}>
+                            {tool}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
