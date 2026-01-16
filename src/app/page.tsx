c8cb6653f97eabf19980322d06981505df6b14fe
nebbilies <hirohamada17092005@gmail.com>
2026-01-16 11:52:46 +0700

    feat: add Project section (wip)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import {motion} from "framer-motion";
 import AboutSection from "@/app/_components/AboutSection/AboutSection";
+import ProjectSection from "@/app/_components/ProjectSection/ProjectSection";
 
 export default function Home() {
   return (
@@ -16,22 +17,23 @@ export default function Home() {
                   backgroundSize: '20px 20px'
               }}
           />
-          <div className="relative h-screen items-center justify-center w-full bg-cover"
+          <div className="relative h-screen items-center justify-center w-full bg-cover bg-center lg:bg-cover"
                style={{backgroundImage: 'url("/image/background/our-identity.gif")'}}>
-              <div className="absolute w-full h-full bg-gradient-to-r from-black/90 from-25% via-black/75 via-50% to-transparent to-100% opacity-90 -z-0"></div>
+              <div className="absolute w-full h-full bg-gradient-to-r from-black/90 from-25% via-black/75 via-50% to-transparent to-100% opacity-90 z-0"></div>
               <div className="text-primary-content w-full h-full flex z-10 relative">
-                  <div className={'w-1/2 flex flex-col justify-center items-center h-full'}>
+                  <div className={'w-full lg:w-1/2 flex flex-col justify-center items-center h-full'}>
                       <div className="max-w-xl">
-                          <h1 className="mb-5 text-7xl font-bold">Hi, <span className={'text-accent'}>Nebby </span> です。</h1>
-                          <h3 className="text-4xl font-bold mb-10">
+                          <h1 className="mb-5 text-5xl font-bold lg:text-7xl">Hi, <span className={'text-accent'}>Nebby </span> です。</h1>
+                          <h3 className="text-2xl font-bold mb-10 lg:text-4xl">
                               I <span className={'text-accent'}>build</span> things for the web.
                           </h3>
-                          <button className="btn btn-primary btn-xl">Get Started</button>
+                          <button className="btn btn-primary btn-xl">Blog</button>
                       </div>
                   </div>
               </div>
           </div>
           <AboutSection/>
+          <ProjectSection/>
       </div>
   );
 }
